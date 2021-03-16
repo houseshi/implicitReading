71abbd183ef0eb880adaf291bc08582355d2a0b7
Administrator <18537159533@139.com>
2021-03-16 15:58:30 +0800

    增加 less 以及其他配置， 初次使用

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,57 +1,83 @@
-import React, { useEffect, useState } from "react";
+import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import "./less/popup.less"
 
-const Popup = () => {
-  const [count, setCount] = useState(0);
-  const [currentURL, setCurrentURL] = useState<string>();
 
-  useEffect(() => {
-    chrome.browserAction.setBadgeText({ text: count.toString() });
-  }, [count]);
 
-  useEffect(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      setCurrentURL(tabs[0].url);
-    });
-  }, []);
+export default class Popup extends Component {
 
-  const changeBackground = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      const tab = tabs[0];
-      if (tab.id) {
-        chrome.tabs.sendMessage(
-          tab.id,
-          {
-            color: "#555555",
-          },
-          (msg) => {
-            console.log("result message:", msg);
-          }
-        );
+  state = []
+  componentDidMount(){
+    let file:any =[];
+    chrome.storage.local.get({"books":false},function(item){
+      if(item.books){
+        file = item.books
       }
     });
-  };
+    this.setState([...file])
+  }
 
-  return (
-    <>
-      <ul style={{ minWidth: "700px" }}>
-        <li>Current URL: {currentURL}</li>
-        <li>Current Time: {new Date().toLocaleTimeString()}</li>
-      </ul>
-      <button
-        onClick={() => setCount(count + 1)}
-        style={{ marginRight: "5px" }}
-      >
-        count up
-      </button>
-      <button onClick={changeBackground}>change background</button>
-    </>
-  );
-};
-
+  onChange = (e:any) =>{
+    const files = e.target.files;
+    chrome.storage.local.set({"books":files},function(){
+      console.log("保存成功")
+    });
+    this.setState([...files])
+    
+  }
+  render() {
+    return (
+      <div> 
+        <input onChange={this.onChange} type="file" name="file" id="file" multiple />
+        <button className="btn">upload</button>
+      </div>
+    )
+  }
+}
 ReactDOM.render(
   <React.StrictMode>
     <Popup />
   </React.StrictMode>,
   document.getElementById("root")
 );
+// const Popup = () => {
+
+
+//   // const [count, setCount] = useState(0);
+//   // const [currentURL, setCurrentURL] = useState<string>();
+
+//   // useEffect(() => {
+//   //   chrome.browserAction.setBadgeText({ text: count.toString() });
+//   // }, [count]);
+
+//   // useEffect(() => {
+//   //   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//   //     setCurrentURL(tabs[0].url);
+//   //   });
+//   // }, []);
+
+//   // const changeBackground = () => {
+//   //   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//   //     const tab = tabs[0];
+//   //     if (tab.id) {
+//   //       chrome.tabs.sendMessage(
+//   //         tab.id,
+//   //         {
+//   //           color: "#555555",
+//   //         },
+//   //         (msg) => {
+//   //           console.log("result message:", msg);
+//   //         }
+//   //       );
+//   //     }
+//   //   });
+//   // };
+
+//   return (
+//       <div>
+//         <input onChange={this.onChange} type="file" name="file" id="file"/>
+//       </div>
+//   );
+// };
+
+
